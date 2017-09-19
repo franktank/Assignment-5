b1cc99c5e4a77b3b678b4b191200486a11efe6d6
Franky Liang <fliangz96@gmail.com>
2017-09-19 17:27:05 -0400

    Change routes

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -1,24 +1,24 @@
-angular.module('listings').factory('Listings', ['$http', 
+angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/listings');
+        return $http.get('http://127.0.0.1:8080/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
-      }, 
+        return $http.post('http://127.0.0.1:8080/api/listings', listing);
+      },
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
-      }, 
+        return $http.get('http://127.0.0.1:8080/api/listings/' + id);
+      },
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
-      }, 
+        return $http.put('http://127.0.0.1:8080/api/listings/' + id, listing);
+      },
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete('http://127.0.0.1:8080/api/listings/' + id);
       }
     };
 
